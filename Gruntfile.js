3b9c8e4c2ebd8807984b80eb21153e55626fefcb
Julián Gutiérrez <juliangut@gmail.com>
2014-10-17 12:59:08 +0200

    qa tools

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        phplint: {
+            options: {
+                swapPath: '/tmp'
+            },
+            application: ['src/*.php']
+        },
+        phpcs: {
+            options: {
+                bin: './vendor/bin/phpcs',
+                standard: './phpcs.xml'
+            },
+            application: {
+                dir: ['./src']
+            }
+        },
+        phpmd: {
+            options: {
+                bin: './vendor/bin/phpmd',
+                rulesets: './phpmd.xml',
+                reportFormat: 'text'
+            },
+            application: {
+                dir: './src'
+            }
+        },
+        phpdcd: {
+            options: {
+                bin: './vendor/bin/phpdcd'
+            },
+            application: {
+                dir: ['src']
+            }
+        },
+        phpunit: {
+            options: {
+                bin: './vendor/bin/phpunit'
+            },
+            application: {
+                configuration: './phpunit.xml'
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['phplint', 'phpcs', 'phpmd', 'phpdcd']);
+};
