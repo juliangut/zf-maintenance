d385d8339d1905000807c6cd60ffa6ecb26f3b30
Julián Gutiérrez <juliangut@gmail.com>
2014-10-17 22:54:40 +0200

    simplify bootstrap

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -6,33 +6,21 @@
  * @license https://raw.githubusercontent.com/juliangut/zf-maintenance/master/LICENSE
  */
 
-namespace JgutZfMaintenance;
-
-use Composer\Autoload\ClassLoader;
-use Zend\Loader\AutoloaderFactory;
-
 $dir = __DIR__;
 $previousDir = '.';
 while (!is_dir($dir . '/vendor')) {
     $dir = dirname($dir);
     if ($previousDir === $dir) {
-        throw new \RuntimeException(
-            'vendor/autoload.php could not be found. Did you run `php composer.phar install`?'
-        );
+        break;
     }
     $previousDir = $dir;
 }
 $vendorPath = $dir . '/vendor';
 
-$loader = null;
 if (is_readable($vendorPath . '/autoload.php')) {
-    $loader = include $vendorPath . '/autoload.php';
+    include $vendorPath . '/autoload.php';
 } elseif (is_readable(dirname($vendorPath) . '/autoload.php')) {
-    $loader = include dirname($vendorPath) . '/autoload.php';
-}
-
-if (!$loader instanceof ClassLoader) {
-    throw new \RuntimeException(
-        'vendor/autoload.php could not be found. Did you run `php composer.phar install`?'
-    );
+    include dirname($vendorPath) . '/autoload.php';
+} else {
+    throw new \RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
