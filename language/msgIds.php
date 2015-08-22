8dace47712969c80ec4245835d6b6eff35cab597
Stijn Haulotte <stijnhau@gmail.com>
2015-08-22 12:25:24 +0200

    Dutch language

---
+++ language/msgIds.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Ids for poedit
+ * @method translate()
+ */
+translate('Undergoing maintenance tasks');
+translate('We are currently running maintenance proccesses');
+translate('File maintenance mode active on Jgut\Zf\Maintenance\Provider\FileProvider');
+
